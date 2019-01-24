dbbbe8f4f789acc25f3f7132b2790e20636ff723
Alexhamagan <45038699+Alexhamagan@users.noreply.github.com>
2019-01-24 15:18:40 +0200

    Update engine.js

--- public/js/engine.js
+++ public/js/engine.js
@@ -2,5 +2,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+ctx.width = 600;
+ctx.length = 600;
+
 ctx.fillStyle = 'green';
 ctx.fillRect(10, 10, 150, 100);
